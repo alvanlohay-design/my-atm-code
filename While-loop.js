69505e53c71845751d82beb71d42ef8e4dd23c0d
Alvan Sebastian <alvanlohay@gmail.com>
2026-06-19 16:47:49 +0300

    first commit

---
+++ While-loop.js
@@ -0,0 +1,23 @@
+let pin=prompt("Enter your pin");
+while(pin !="1234"){
+    alert("Incorrect pin");
+    pin=prompt("Try again")
+}
+alert("Welcome to the bank Alvan");
+let balance=5000;
+alert("Your balance is " +balance);
+let withdrawal=Number(prompt("How much do you want to withdraw?"));
+if (withdrawal<=balance&&withdrawal>0){
+    balance=balance-withdrawal;
+    alert("Withdrawal successful");
+    alert("You withdrew "+withdrawal);
+    alert("Your remaning balance is " +balance);
+}
+else if(withdrawal>balance||withdrawal<0){
+    alert("Write the correct amount");
+    withdrawal=Number(prompt("How much do you want to withdraw?"));
+}
+else{
+    alert("Insufficient money or invalid amount");
+}
+alert("Thank you for using our bank Alvan.");
